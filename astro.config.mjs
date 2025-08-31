d78cfb49919c149d299b51224d255b5f648696fc
Nyeng <37041460+humanmalabag@users.noreply.github.com>
2025-08-31 16:36:44 +0000

    Initial config

---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightThemeFlexoki from 'starlight-theme-flexoki'
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://humanmalabag.github.io',
+	integrations: [
+		starlight({
+			plugins: [starlightThemeFlexoki()],
+			title: 'My Docs',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
